75d7e4bcaed030394fb8cf285fff18d6e21da742
Micaella Colombo <micaelc@g.clemson.edu>
2021-10-17 19:30:57 -0400

    commit tree 8e11aadb6e7867e26f4ed5775b5f451300cfa667

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,18 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :super_hero
+    end
 
+    post '/teams' do
+        @team = Team.new(params[:team])
+
+        params[:team][:members].each do |hero|
+            Superhero.new(hero)
+        end
+
+        @superhero = Superhero.all
+        erb :team
+    end
 end
